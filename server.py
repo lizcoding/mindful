8d74e48e74bb3f91c2d13dd2f95b4dfb039db463
lizcoding <elizarn96@gmail.com>
2021-12-19 21:01:39 -0800

    Modify project file structure and edit file content

---
+++ server.py
@@ -0,0 +1,39 @@
+from flask import Flask, redirect, request, render_template, session
+from jinja2 import StrictUndefined
+
+
+app = Flask(__name__)
+app.jinja_env.undefined = StrictUndefined
+app.jinja_env.auto_reload = True
+
+# Required to use Flask sessions
+app.secret_key = "MINDFUL"
+
+
+@app.route("/")
+def show_login():
+    return render_template("login.html")
+
+
+@app.route("/dashboard")
+def show_dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/profile")
+def show_profile():
+    return render_template("profile.html")
+
+
+@app.route("/item/<int:item_id>")
+def focus_item(item_id):
+    return render_template("item.html", item=item_id)
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        use_reloader=True,
+        use_debugger=True,
+    )
+
